779ce39a57db9cc2e584e687254244b2610b2c35
gimoz71 <gianluca.monti@ecoseekr.com>
2015-04-04 01:02:54 +0200

    gestioen favicons

--- index.php
+++ index.php
@@ -13,8 +13,25 @@
         <meta property="og:image:width"     content="500" />
         <meta property="og:image:height"    content="500" />
         <meta property="og:description"    content=" AppleForFree.com ti permette di ricevere gratuitamente ed in poco tempo uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nostro sito. Per ottenerli basta semplicemente  accedere ed usufruire  degli accessori oppure dei servizi offerti nel markeplace." />
+        <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="images/favicon/android-chrome-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="images/favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href=favicon.ico" type="image/x-icon">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#2b5797">
+        <meta name="msapplication-TileImage" content="images/mstile-144x144.png">
+        <meta name="theme-color" content="#ffffff">
         <link href="css/soon.css" media="screen" rel="stylesheet" type="text/css">
         <style type="text/css">
             .clearfix:after {
