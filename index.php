d2a87af711c7fe37277770e906622023a88d2c77
Gianluca <gm3871@gmail.com>
2015-04-03 19:06:19 +0200

    primi test

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AppleForFree.com - La Mela Diventa per Tutti</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href=favicon.ico" type="image/x-icon">
+        <link href="css/OLD/main.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="css/OLD/button.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="css/OLD/utility.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="css/OLD/animate.css" media="screen" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            .clearfix:after {
+                content: ".";
+                display: block;
+                height: 0;
+                clear: both;
+                visibility: hidden;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <div class="menu">www.appleforfree.com</div>
+            <h1 class="logo">AFF</h1>
+        </header>
+        <div class="body">
+            <h2>La mela diventa per tutti!</h2>
+            <p>AppleForFree.com ti permette di <stron>ricevere gratuitamente ed in poco tempo</stron> uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nosrtro sito.<br>Per ottenerli basta semplicemente accedere ed usufruire degli accessori oppure dei servizi offerti nel marketplace.</p>
+        </div>
+        <footer>
+            <p>Seguici su Facebook per essere uno dei primi utenti felici</p>
+            <p>Stay tuned></p>
+        </footer>
+    </body>
+</html>
