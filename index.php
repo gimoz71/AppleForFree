f766f8f2d4cc8eeaa70f78617f2b450f5e8a37cf
gimoz71 <gianluca.monti@ecoseekr.com>
2015-04-03 23:06:41 +0200

    init index page

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="fb:app_id"          content="1496481453932000" />
+        <meta property="og:type"            content="product" />
+        <meta property="og:url"             content="http://www.appleforfree.com" />
+        <meta property="og:title"           content="La mela diventa per tutti" />
+        <meta property="og:image"           content="https://scontent-mxp.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10606205_687341228044844_2738984387206199640_n.jpg?oh=cbf3a735bee692360602e65b64b30ba8&oe=55A43ED6" />
+        <meta property="og:description"    content=" AppleForFree.com ti permette di ricevere gratuitamente ed in poco tempo uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nostro sito. Per ottenerli basta semplicemente  accedere ed usufruire  degli accessori oppure dei servizi offerti nel markeplace." />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href=favicon.ico" type="image/x-icon">
-        <link href="css/OLD/main.css" media="screen" rel="stylesheet" type="text/css">
-        <link href="css/OLD/button.css" media="screen" rel="stylesheet" type="text/css">
-        <link href="css/OLD/utility.css" media="screen" rel="stylesheet" type="text/css">
-        <link href="css/OLD/animate.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="css/soon.css" media="screen" rel="stylesheet" type="text/css">
         <style type="text/css">
             .clearfix:after {
                 content: ".";
@@ -22,17 +25,27 @@
         </style>
     </head>
     <body>
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&appId=233242826685913&version=v2.3";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
         <header>
-            <div class="menu">www.appleforfree.com</div>
+            <div class="menu">www.<strong>apple</strong>forfree.com</div>
             <h1 class="logo">AFF</h1>
-        </header>
-        <div class="body">
             <h2>La mela diventa per tutti!</h2>
+        </header>
+        <div class="content">
             <p>AppleForFree.com ti permette di <stron>ricevere gratuitamente ed in poco tempo</stron> uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nosrtro sito.<br>Per ottenerli basta semplicemente accedere ed usufruire degli accessori oppure dei servizi offerti nel marketplace.</p>
         </div>
         <footer>
             <p>Seguici su Facebook per essere uno dei primi utenti felici</p>
-            <p>Stay tuned></p>
+            <div style="margin: 10px 0;" class="fb-like" data-href="http://www.appleforfree.com/" data-width="50" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
+            <p>Stay tuned</p>
         </footer>
     </body>
 </html>
