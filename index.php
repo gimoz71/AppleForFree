bed038551380abb8e0d83132de0e4c0eb52858b5
gimoz71 <gianluca.monti@ecoseekr.com>
2015-04-03 23:21:05 +0200

    tuning fb like image

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>AppleForFree.com - La Mela Diventa per Tutti</title>
-        <meta name="description" content="">
+        <meta name="description" content=" AppleForFree.com ti permette di ricevere gratuitamente ed in poco tempo uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nostro sito. Per ottenerli basta semplicemente  accedere ed usufruire  degli accessori oppure dei servizi offerti nel markeplace.">
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="fb:app_id"          content="1496481453932000" />
@@ -10,6 +10,8 @@
         <meta property="og:url"             content="http://www.appleforfree.com" />
         <meta property="og:title"           content="La mela diventa per tutti" />
         <meta property="og:image"           content="http://www.appleforfree.com/images/fb_like_logo.jpg" />
+        <meta property="og:image:width"     content="500" />
+        <meta property="og:image:height"     content="500" />
         <meta property="og:description"    content=" AppleForFree.com ti permette di ricevere gratuitamente ed in poco tempo uno dei tanti prodotti Apple, nuovi ed originali, offerti nelle vetrine dei siti web del nostro sito. Per ottenerli basta semplicemente  accedere ed usufruire  degli accessori oppure dei servizi offerti nel markeplace." />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href=favicon.ico" type="image/x-icon">
@@ -36,7 +38,7 @@
         </script>
         <header>
             <div class="menu">www.<strong>apple</strong>forfree.com</div>
-            <h1 class="logo">AFF</h1>
+            <h1 class="logo">AppleForFree.com</h1>
             <h2>La mela diventa per tutti!</h2>
         </header>
         <div class="content">
