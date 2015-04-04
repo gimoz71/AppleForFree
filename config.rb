90fb8cfa09b789d4f14fe42f08998b120f6203a7
gimoz71 <gianluca.monti@ecoseekr.com>
2015-04-04 11:23:31 +0200

    testing

--- config.rb
+++ config.rb
@@ -1,9 +1,8 @@
-
-css_dir = "css" # by Compass.app 
+css_dir = "css" # by Compass.app
 sass_dir = "scss" # by Compass.app 
 images_dir = "images" # by Compass.app 
 output_style = :expanded # by Compass.app 
-relative_assets = true # by Compass.app 
+relative_assets = false # by Compass.app
 line_comments = true # by Compass.app 
 sass_options = {:debug_info=>true} # by Compass.app 
-sourcemap = true # by Compass.app 
+sourcemap = false # by Compass.app
