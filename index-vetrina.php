ea0cc6b8dba404a42b7516a0d430bfedf18d75bd
Gianluca <gm3871@gmail.com>
2015-04-15 12:46:48 +0200

    fix footer responsive

--- index-vetrina.php
+++ index-vetrina.php
@@ -212,14 +212,18 @@
         </div>
     </div> <!-- /container -->
     <footer class="footer">
-        <div class="container">
-            <span class="navbar-text pull-left"><b>Apple</b>ForFree</span>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"  class="navbar-link">Network</a></li>
-                <li><a href="#">Soluzioni pubblicitarie</a></li>
-                <li><a href="#">Programma di affiliazione</a></li>
-                <li><a href="#">Contatti</a></li>
-            </ul>
+        <div class="container text-center">
+            <div class="col-md-3 col-sm-12">
+                <span class="navbar-text"><b>Apple</b>ForFree</span>
+            </div>
+            <div class="col-md-9 col-sm-12">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"  class="navbar-link">Network</a></li>
+                    <li><a href="#">Soluzioni pubblicitarie</a></li>
+                    <li><a href="#">Programma di affiliazione</a></li>
+                    <li><a href="#">Contatti</a></li>
+                </ul>
+            </div>
         </div>
     </footer>
 
