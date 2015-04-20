68860821cf61ab6c79ed558af97a4e9c1c5a372a
Gianluca <gm3871@gmail.com>
2015-04-20 14:36:58 +0200

    fix

--- index-principale.php
+++ index-principale.php
@@ -53,128 +53,20 @@
 <body role="document">
 
     <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="header-logo" href="#">AppleForFree</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Come funziona</a></li>
-                    <li><a href="#testimonianze">Testimonianze</a></li>
-                    <li><a href="#about">Faq</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">login <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Inglese</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Italiano <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Inglese</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </nav>
-    <nav class="product-navbar">
-        <div class="container">
-            <a class="products-navbar-product"><span>5</span>iPhone</a>
-            <a class="products-navbar-product"><span>6</span>iPad</a>
-            <a class="products-navbar-product"><span>7</span>MacBook</a>
-            <a class="products-navbar-product"><span>8</span>iMac</a>
-            <a class="products-navbar-product"><span>5</span>icona</a>
-            <a class="products-navbar-product"><span>5</span>icona</a>
-        </div>
-    </nav>
-
+    <?php include '_include/nav.php'; ?>
     <div class="container theme-showcase text-xs-center text-md-left" role="main">
         <div class="text-center text-muted">
             <h1>Regalati gratis il tuo nuovo Apple</h1>
         </div>
-        <div class="product-highlight">
-            <div class="col-md-6">
-                <img src="images/iphone6.png" alt="img" class="img-responsive center-block">
-            </div>
-            <div class="col-md-6">
-                <div class="product-highlight-description">
-                    <div class="col-md-12">
-                        <h2><b>Apple iPhone 6</b></h2>
-                    </div>
-                    <div class="col-md-6">
-                        <h4>Capienza 64 GB</h4>
-                        <h4>Software IOS 8</h4>
-                        <h4>Chip A8, M8</h4>
-                        <h4>Display 4,7 Inch</h4>
-                        <h4>Capienza 64 GB</h4>
-                    </div>
-                    <div class="col-md-6">
-                        <h4>LTE</h4>
-                        <h4>Retina Display</h4>
-                        <h4>Touch ID</h4>
-                        <h4>Wi‑Fi 802.11a/b/g/n/ac</h4>
-                        <h4>Bluetooth 4.0</h4>
-                    </div>
-                    <div class="col-md-12">
-                        <hr>
-                        <p class="text-muted"><small>iPhone 6 non è soltanto più grande: è ancora migliore, da ogni punto di vista. Più largo, ma decisamente più sottile. Più potente, ma anche più efficiente nei consumi. Un’avvolgente superficie in metallo che diventa tutt’uno con il nuovo display Retina HD. Un design in cui hardware e software si completano e si integrano alla perfezione, dando vita a una nuova generazione di iPhone.</small></p>
-                        <p>Visita Apple Store per maggiori informazioni su iPhone 6</p>
-                        <hr>
-                    </div>
-                    <div class="col-md-6">
-                        <h1><small>Prezzo</small> <del>799€</del></h1>
-                    </div>
-                    <div class="col-md-6">
-                        <h1><small>prendilo</small> Gratis!</h1>
-                    </div>
-                    <div class="col-md-12">
-                        <hr>
-                        <h5>Registrati subito per ricevere il tuo nuovo iPhone 6</h5>
-                        <a class="btn btn-social btn-facebook">
-                            <i class="fa fa-facebook"></i> Registrati con Facebook
-                        </a>
-                        <div class="spacer10 hidden-lg hidden-md hidden-sm"></div>
-                        <a class="btn btn-social btn-twitter">
-                            <i class="fa fa-twitter"></i> Registrati con Twitter
-                        </a>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
         <hr>
-        <div class="page-header">
-            <h4>Altri prodotti Apple in promozione</h4>
-        </div>
         <div class="other-products">
             <div class="col-sm-6 col-md-3">
                 <div class="text-center">
                     <img src="holder.js/100%x200" alt="img">
                     <div class="caption">
                         <h4>iMac Retina 5k</h4>
-                        <h4><del>1.245 €</del></h4>
-                        <h4><em>Free</em></h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
                     </div>
                 </div>
             </div>
@@ -183,8 +75,8 @@
                     <img src="holder.js/100%x200" alt="img">
                     <div class="caption">
                         <h4>iMac Retina 5k</h4>
-                        <h4><del>1.245 €</del></h4>
-                        <h4><em>Free</em></h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
                     </div>
                 </div>
             </div>
@@ -193,8 +85,8 @@
                     <img src="holder.js/100%x200" alt="img">
                     <div class="caption">
                         <h4>iMac Retina 5k</h4>
-                        <h4><del>1.245 €</del></h4>
-                        <h4><em>Free</em></h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
                     </div>
                 </div>
             </div>
@@ -203,40 +95,136 @@
                     <img src="holder.js/100%x200" alt="img">
                     <div class="caption">
                         <h4>iMac Retina 5k</h4>
-                        <h4><del>1.245 €</del></h4>
-                        <h4><em>Free</em></h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
                     </div>
                 </div>
             </div>
         </div>
         <!-- Main jumbotron for a primary marketing message or call to action -->
-        <div class="reg-action text-center">
-            <h1>Registrati subito per ricevere gratis il tuo nuovo Apple Iphone 6</h1>
-            <a class="btn btn-social btn-facebook">
-                <i class="fa fa-facebook"></i> Registrati con Facebook
-            </a>
-            <div class="spacer10 hidden-lg hidden-md hidden-sm"></div>
-            <a class="btn btn-social btn-twitter">
-                <i class="fa fa-twitter"></i> Registrati con Twitter
-            </a>
-        </div>
+        <?php include '_include/call_to_action.php' ?>
     </div> <!-- /container -->
-    <footer class="footer">
-        <div class="container text-center">
-            <div class="col-md-3 col-sm-12">
-                <span class="navbar-text"><b>Apple</b>ForFree</span>
-            </div>
-            <div class="col-md-9 col-sm-12">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"  class="navbar-link">Network</a></li>
-                    <li><a href="#">Soluzioni pubblicitarie</a></li>
-                    <li><a href="#">Programma di affiliazione</a></li>
-                    <li><a href="#">Contatti</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
-
+    <?php include '_include/footer.php' ?>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
