0e5f375ba56b045a7bb94f23ee0c97d72041d071
Gianluca <gm3871@gmail.com>
2015-04-20 14:27:57 +0200

    creazione index-principale.php e inizio scorporamento elementi comuni alle pagine

---
+++ index-principale.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Nuovo e gratis su AppleForFree.com!!! Con il suo innovativo design avvolgente, il display più evoluto di sempre e tanto altro, iPhone 6 è molto più che più grande.">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>AppleForFree.com - Regalati Gratis il tuo nuovo iPhone 6 64Gb</title>
+    <meta property="fb:app_id"          content="1496481453932000" />
+    <meta property="og:type"            content="product" />
+    <meta property="og:url"             content="http://www.appleforfree.com/Apple-iPhone-6-64gb" />
+    <meta property="og:title"           content="AppleForFree.com - Regalati Gratis il tuo nuovo iPhone 6 64Gb" />
+    <meta property="og:image"           content="http://www.appleforfree.com/images/fb_like_logo.jpg" />
+    <meta property="og:image:width"     content="500" />
+    <meta property="og:image:height"    content="500" />
+    <meta property="og:description"    content="Nuovo e gratis su AppleForFree.com!!! Con il suo innovativo design avvolgente, il display più evoluto di sempre e tanto altro, iPhone 6 è molto più che più grande." />
+    <!-- Custom styles for this template -->
+    <link href="stylesheets/styles.css" rel="stylesheet">
+    <link href="stylesheets/sb-admin-2.css" rel="stylesheet">
+    <!-- Optional theme -->
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">-->
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 30px;
+        }
+
+        .theme-dropdown .dropdown-menu {
+            position: static;
+            display: block;
+            margin-bottom: 20px;
+        }
+
+        .theme-showcase > p > .btn {
+            margin: 5px 0;
+        }
+
+        .theme-showcase .navbar .container {
+            width: auto;
+        }
+
+    </style>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body role="document">
+
+    <!-- Fixed navbar -->
+    <?php include '_include/nav.php'; ?>
+    <div class="container theme-showcase text-xs-center text-md-left" role="main">
+        <div class="text-center text-muted">
+            <h1>Regalati gratis il tuo nuovo Apple</h1>
+        </div>
+        <hr>
+        <div class="other-products">
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="text-center">
+                    <img src="holder.js/100%x200" alt="img">
+                    <div class="caption">
+                        <h4>iMac Retina 5k</h4>
+                        <h4><del class="text-muted">1.245 €</del> <b>Free</b></h4>
+                        <h4><b>Delivery list 20</b></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <?php include '_include/call_to_action.php' ?>
+    </div> <!-- /container -->
+    <?php include '_include/footer.php' ?>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="javascripts/bootstrap.js"></script>
+    <script src="javascripts/holder.js"></script>
+    <script>
+        $('.dropdown-toggle').dropdown()
+    </script>
+</body>
+</html>
+
